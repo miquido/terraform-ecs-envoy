bd7fbdbbe93edcc4b11f86c163167e756c93f2fa
Marek Mościchowski <marek.moscichowski@miquido.com>
2021-05-28 13:09:05 +0000

    Feat(ci): Dependabot

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -3,5 +3,12 @@ provider "aws" {
 }
 
 module "test" {
-  source = "../../"
+  source                            = "../../"
+  app-ports                         = 8080
+  appmesh-resource-arn              = "arn::..."
+  aws_service_discovery_service_arn = "arn::..."
+  awslogs-group                     = "testgroup"
+  awslogs-region                    = "eu-west-1"
+  container_name                    = "test"
+  egress-ignored-ports              = 5432
 }
